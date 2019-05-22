26580c230b16e9d3f70b2905e24512d16eef4fab
JavierSLX <jserranolule@gmail.com>
2019-05-21 19:13:36 -0500

    Clase terminada

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const logger = require('morgan'); //Morgan es middleware tipo logger
+const bodyParser = require('body-parser'); //Para peticiones http
 const app = express();
+
+//Se agregan los middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 require("./routes/views")(app);
 require("./routes/special")(app);
